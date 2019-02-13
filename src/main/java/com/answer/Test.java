0b13e16adbcad4d71b2d4f68b255461cf1287972
suchao <persistsuchao@hotmail.com>
2019-02-14 00:37:14 +0800

    添加依赖

--- src/main/java/com/answer/Test.java
+++ src/main/java/com/answer/Test.java
@@ -1,7 +1,17 @@
 package com.answer;
 
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+
 public class Test {
+
+    private static Log log = LogFactory.get(Test.class);
+
     public static void main(String[] args) {
+        //测试 grep console
         System.out.println("this is java review");
+        log.info("log info level");
+        log.debug("log debug level");
+        log.error("log error level");
     }
 }
